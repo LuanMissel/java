45898e513fe7aff76e1e4b044cd7792d04856816
Luan Missel <luan.missel.lourenco@gmail.com>
2025-08-23 17:54:45 -0300

    Initial commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+public class PrimeiroPrograma{
+    public static void main(String[] args) {
+        
+    }    
+}
+/* se observar as principais palavras, você irá ver que é um metódo principal, dentro da classe primeiro programa, que faz parte de um pacote primeiro programa. */
+
+
+    
+
+ 
