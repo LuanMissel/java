131a2ac027af76c5fbc2f59cbac843e4545695e2
Luan Missel <luan.missel.lourenco@gmail.com>
2025-08-23 18:54:49 -0300

    second change

--- src/PrimeiroPrograma.java
+++ src/PrimeiroPrograma.java
@@ -1,7 +1,9 @@
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
-    }    
+        System.out.println("hello world!");
+        System.out.println("Curso do Guanabara");
+        System.out.println("Aprendendo Java definitivamente");    
+    }
 }
 /* se observar as principais palavras, você irá ver que é um metódo principal (main), dentro da classe primeiro programa (class), que faz parte de um pacote primeiro programa.(package)*/
 
