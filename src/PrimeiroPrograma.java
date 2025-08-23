98e658dd3e3755a549d7a048a6dfb93419fd0e0e
Luan Missel <luan.missel.lourenco@gmail.com>
2025-08-23 18:25:05 -0300

    Required changes

--- src/PrimeiroPrograma.java
+++ src/PrimeiroPrograma.java
@@ -1,9 +1,9 @@
-public class PrimeiroPrograma{
+public class PrimeiroPrograma {
     public static void main(String[] args) {
-        
+        System.out.println("Olá, mundo!");
     }    
 }
-/* se observar as principais palavras, você irá ver que é um metódo principal, dentro da classe primeiro programa, que faz parte de um pacote primeiro programa. */
+/* se observar as principais palavras, você irá ver que é um metódo principal (main), dentro da classe primeiro programa (class), que faz parte de um pacote primeiro programa.(package)*/
 
 
     
