dc6bdf26ce4404411c00cb5ea3fcf0ed462f3530
Luan Missel <luan.missel.lourenco@gmail.com>
2025-08-25 22:58:07 -0300

    Adiciona Aula02

---
+++ maratona-java/src/academy/devdojo/maratonajava/introdução/Aula01OlaDevDojo.java
@@ -0,0 +1,8 @@
+// A primeira linha que não for comentário, tem que ser o package.
+package academy.devdojo.maratonajava.introdução;
+
+public class Aula01OlaDevDojo {
+    public static void main (String[] args) {
+        System.out.println("KA-ME-HA-ME-HAA");
+    }
+}
