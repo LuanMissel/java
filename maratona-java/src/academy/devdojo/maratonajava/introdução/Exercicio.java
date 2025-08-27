5825e5f4e55d2d61e93dde322b072756da77f9e0
Luan Missel <luan.missel.lourenco@gmail.com>
2025-08-26 23:07:53 -0300

    Adiciona Exercício

--- maratona-java/src/academy/devdojo/maratonajava/introdução/Exercicio.java
+++ maratona-java/src/academy/devdojo/maratonajava/introdução/Exercicio.java
@@ -1,5 +1,13 @@
-package src.academy.devdojo.maratonajava.introdução;
+package src.academy.devdojo.maratonajava.introdução.main;
 
 public class Exercicio {
-
+    public static void main (String[] args) {
+    String name = "Luan";
+    int age = 17;
+    char sex = 'M';
+    String color ="blue";
+    System.out.println("Olá Mundo, eu me chamo" + name + "tenho" + age + "e sou do sexo" + sex);
+    System.out.println("Hello world, my name is " + name + " I'm " + age + " years old and I am " + sex);
+        System.out.println("I'm " + name + " and my favorite color is " + color);
+    }
 }
