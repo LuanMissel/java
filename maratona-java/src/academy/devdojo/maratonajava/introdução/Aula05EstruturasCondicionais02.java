d6124565cbfa837e98b5932ace35f87302d38031
Luan Missel <luan.missel.lourenco@gmail.com>
2025-08-29 21:25:49 -0300

    adiciona operações condicionais 2 e 3

---
+++ maratona-java/src/academy/devdojo/maratonajava/introdução/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,24 @@
+package src.academy.devdojo.maratonajava.introdução;
+
+import java.util.Scanner;
+
+public class Aula05EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        Scanner age = new Scanner(System.in);
+       //  idade < 15 categoria juvenil
+       //  idade >= 15 && idade < 18 categoria de base
+       // idade >= 18 categoria profissional
+        System.out.println("Qual a sua idade? ");
+        int idade = age.nextInt();
+        String categoria;
+        if (idade < 15) {
+            categoria = ("Categoria infantil!");
+        } else if (idade >= 15 && idade < 18) {
+            categoria = ("Categoria de base!");
+        } else {
+            categoria = ("Categoria profissional!");
+        }
+        System.out.println(categoria);
+
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/introdução/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,16 @@
+package src.academy.devdojo.maratonajava.introdução;
+
+import java.util.Scanner;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        Scanner pay = new Scanner(System.in);
+        System.out.println("Qual o seu salário? ");
+        double salario = pay.nextDouble();
+        String mensagemDoar = "eu vou doar 500 por DevDojo";
+        String mensagemNaoDoar = "ainda não tenho condições, mas vou ter!";
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+        System.out.println(resultado);
+    }
+}
