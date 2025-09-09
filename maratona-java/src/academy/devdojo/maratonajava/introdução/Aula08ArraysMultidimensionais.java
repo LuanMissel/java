f333cff115d6664eac1aaa592063814b144c3596
Luan Missel <luan.missel.lourenco@gmail.com>
2025-09-08 21:15:16 -0300

    adiciona arrays multidimensionais

--- maratona-java/src/academy/devdojo/maratonajava/introdução/Aula08ArraysMultidimensionais.java
+++ maratona-java/src/academy/devdojo/maratonajava/introdução/Aula08ArraysMultidimensionais.java
@@ -1,4 +1,30 @@
-package src.academy.devdojo.maratonajava.introdução;
-
-public class Aula08ArraysMultidimensionais {
+    package src.academy.devdojo.maratonajava.introdução;
+    public class Aula08ArraysMultidimensionais {
+    public static void main(String[] args) {
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+        
+        dias[1][0] = 30;
+        dias[1][1] = 31;
+        dias[1][2] = 30;
+        
+        dias[2][0] = 31;
+        dias[2][1] = 30;
+        dias[2][2] = 31;
+        
+        /*  for (int i = 0; i < dias.length; i++) {
+                for (int j = 0; j < dias[0].length; j++) {
+                    System.out.println(dias[i][j]); 
+                }
+            }
+        */
+        for (int[] arrBase : dias) { 
+                for (int num : arrBase) {
+                System.out.println(num);
+                }
+            }
+        }
+        
 }
--- maratona-java/src/academy/devdojo/maratonajava/introdução/Aula08ArraysMultidimensionais02.java
+++ maratona-java/src/academy/devdojo/maratonajava/introdução/Aula08ArraysMultidimensionais02.java
@@ -1,4 +1,19 @@
 package src.academy.devdojo.maratonajava.introdução;
 
 public class Aula08ArraysMultidimensionais02 {
+    public static void main(String[] args) {
+        int numbers[][] = new int [3][];
+        
+        numbers[0] = new int[6];
+        numbers[1] = new int [4];
+        numbers[2] = new int [3];
+
+        for (int[] num : numbers) {
+            System.out.println("\n ------------");
+            for(int i : num) {
+                System.out.print(i);
+            }
+        }
+
+    }
 }
