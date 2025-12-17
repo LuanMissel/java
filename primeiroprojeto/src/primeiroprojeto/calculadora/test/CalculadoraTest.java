3313f5086325a6b16d5cea2843d1f95a86ea7258
Luan Missel <luan.missel.lourenco@gmail.com>
2025-12-17 19:46:38 -0300

    atualiza primeiro projeto

--- primeiroprojeto/src/primeiroprojeto/calculadora/test/CalculadoraTest.java
+++ primeiroprojeto/src/primeiroprojeto/calculadora/test/CalculadoraTest.java
@@ -7,35 +7,37 @@ import java.util.Scanner;
 public class CalculadoraTest {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        Scanner numeros = new Scanner(System.in);
-        int opcao = 0;
-        while (opcao != 5) {
-            System.out.println("====Calculadora====");
-            System.out.println("1. Soma");
-            System.out.println("2. Subtrair");
-            System.out.println("3. Multiplicar");
-            System.out.println("4. Dividir");
-            System.out.println("5. Sair");
-            System.out.println("Escolha uma opção: ");
+        try {
+            Scanner numeros = new Scanner(System.in);
+            int opcao = 0;
+            while (opcao != 5) {
+                System.out.println("====Calculadora====");
+                System.out.println("1. Soma");
+                System.out.println("2. Subtrair");
+                System.out.println("3. Multiplicar");
+                System.out.println("4. Dividir");
+                System.out.println("5. Sair");
+                System.out.println("Escolha uma opção: ");
 
-            opcao = numeros.nextInt();
+                opcao = numeros.nextInt();
 
-            if (opcao >= 1 && opcao <= 4) {
-                System.out.println("Digite o primeiro valor: ");
-                calculadora.setNum1(numeros.nextDouble());
-                System.out.println("Digite o segundo valor: ");
-                calculadora.setNum2(numeros.nextDouble());
-                switch (opcao) {
-                    case 1 -> calculadora.somar();
-                    case 2 -> calculadora.subtrair();
-                    case 3 -> calculadora.mutiplicar();
-                    case 4 -> calculadora.dividir();
+                if (opcao >= 1 && opcao <= 4) {
+                    System.out.println("Digite o primeiro valor: ");
+                    calculadora.setNum1(numeros.nextDouble());
+                    System.out.println("Digite o segundo valor: ");
+                    calculadora.setNum2(numeros.nextDouble());
+                    switch (opcao) {
+                        case 1 -> calculadora.somar();
+                        case 2 -> calculadora.subtrair();
+                        case 3 -> calculadora.mutiplicar();
+                        case 4 -> calculadora.dividir();
+                    }
+                    calculadora.imprimir();
                 }
-                calculadora.imprimir();
             }
+        } finally {
+            System.out.println("Progama Finalizado!");
         }
-        System.out.println("Progama Finalizado!");
-        numeros.close();
     }
 }
 
